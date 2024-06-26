e4e7d753453d962bffb25b521d4744f2b2761a02
aman-dps1005 <deviprasadsatapathy1005@gmail.com>
2024-06-26 14:12:03 +0530

    rectified the webhook url

--- apps/user_app/lib/actions/createOnrampTransactions.ts
+++ apps/user_app/lib/actions/createOnrampTransactions.ts
@@ -30,7 +30,7 @@ export const createOnrampTransaction =async (amount:number,provider:string)=>{
     })
 
     //hit your webhook
-    const DBupdate=await axios.post("http://3.110.209.219:3000/",{
+    const DBupdate=await axios.post("http://3.110.209.219:3000/hdfcwebhook",{
         token:token,
         user_identifier:userId,
         amount:amount*100
